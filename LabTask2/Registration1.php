091928e8509ac6955a5d81d86f908e28771925b7
Ananyachy2001 <53276401+Ananyachy2001@users.noreply.github.com>
2021-02-14 19:21:39 +0600

    Update Registration1.php

--- LabTask2/Registration1.php
+++ LabTask2/Registration1.php
@@ -17,35 +17,35 @@ $name = $email =$username = $password = $confirmPassword = $gender="";
 $v1="";
 $v2="";
 $v3="";
-if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $name=$_REQUEST["fname"];
+        $name=$_REQUEST["firstname"];
         $email=$_REQUEST["email"];
         $username=$_REQUEST["username"];
-if(empty($_POST["firstname"]) ||! preg_match("/^[a-zA-Z ]*$/",$name){
+        
+if(empty($name) || !preg_match("/^[a-zA-Z ]*$/",$name)){
 
-    $validatename = "Valid Name is Required."
-
+    $validatename = "Valid Name is Required.";
 }
-else{
-    $validatename = "Your Name is".$name;
+else
+{
+    $validatename="Your Name is".$name;
  }
 
-if(empty($_POST["email"]) ||! preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+*@([a-z0-9\'-]+\.)+[a-z]{2.6}$/1x".$email)){
+if(empty($email) || !preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2.6}$/ix",$email)){
 
-    $validateemail = "Valid Email is Requied"
+    $validateemail = "Valid Email is Requied";
 }
 else{
     $validateemail="Your Email is".$email;
 }
 
 
-if(empty($password) || (strlen($password))<6 || !preg_match("/^([@#$%]$/",$password))
+if(empty($password) || (strlen($password))<6 || !preg_match("/^([a-z0-9\+_\-]+)*@#$%([a-z0-9\-]$/ix",$password))
     {
        $validatepassword = "Invalid password.";
     }
-    if(empty($username)|| (strlen($username))<5){
+    if(empty($username) || (strlen($username))<5){
       $validateusername = "Invalid user name";
     }
     else{
@@ -55,53 +55,53 @@ if(empty($password) || (strlen($password))<6 || !preg_match("/^([@#$%]$/",$passw
        $validateconfirmPassword = "Password doesn't match with the previous one.";
     }
 
-if(isset($_REQUEST["vehicle1"]) &&! isset($_REQUEST["vehicle2"])  &&! isset($_REQUEST["vehicle3"])){
+    if(isset($_REQUEST["gender"])){
 
-    echo "please select one";
-}
-else{
-    if(isset($_REQUEST["vehicle1"])){
-        $v1= $_REQUEST["vehicle1"];
+        $validategender= $_REQUEST["gender"];
     }
-    if(isset($_REQUEST["vehicle2"])){
-        $v2= $_REQUEST["vehicle2"];
+    else{
+       echo $validategender= "Please select atleast one";
     }
-    if(isset($_REQUEST["vehicle3"])){
-        $v3= $_REQUEST["vehicle3"];
+    
     }
 
-}
 
-
-}
-<form>
+?>
+<form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
 <table>
 <tr> <td> Name  </td>
-<td> : <input type="text" name="firstname"><?php echo $validatename; ?> </td> </tr>
+<td> : <input type="text" name="firstname"></td><?php echo $validatename; ?> </tr>
 <br>
 <br>
 <tr> 
 <td> Email </td>
-<td> : <input type="text" name="email"><?php echo $validateemail; ?> </td> </tr>
+<td> : <input type="text" name="email"></td><?php echo $validateemail; ?> </tr>
+<br>
+<br>
 <tr> <td> User Name  </td>
-<td> : <input type="text" name="username"></td> </tr>
+<td> : <input type="text" name="username"></td><?php echo $validateusername; ?>  </tr>
 <br>
 <br>
 <tr> 
 <td> Password </td>
-<td> : <input type="password" name="password"> </td> </tr>
+<td> : <input type="password" name="password"></td><?php echo $validatepassword; ?></tr>
 <br>
 <br>
 <tr> 
 <td> Confirm Password </td>
-<td> : <input type="password" name="password"> </td> </tr>
+<td> : <input type="password" name="confirmpassword"></td><?php echo $validateconfirmPassword; ?> </tr>
 <br>
 <br>
 <tr>
 <td> Gender : </td>
-<td>  <input type="radio" id="male" name="gender" value="male">Male 
-<input type="radio" id="female" name="gender" value="female">Female  
-<input type="radio" id="other" name="gender" value="other">Other </td> </tr>
+<td>  <input type="radio" id="male" name="gender" value="Gender:male">
+<label for="male">Male</label> <br>
+<input type="radio" id="female" name="gender" value="Gender:female">
+<label for="female">Female</label> <br>
+<input type="radio" id="other" name="gender" value="Gender: Other"> 
+<label for="other">Other</label> <br>
+
+</td><?php echo $validategender; ?> </tr>
 
 
 <tr>
